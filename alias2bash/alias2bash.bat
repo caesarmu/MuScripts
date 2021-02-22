1c4ac23cfece58c1c10ec1c1cdb1cb98f9ec950a
Mu <caesarmu@users.noreply.github.com>
2021-02-22 10:03:34 +0800

    Add files via upload

---
+++ alias2bash/alias2bash.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+
+REM HELP ==========================DOSKEY CMDs=================================
+REM Unix-like commands 
+IF "x%1" == "x" (ECHO "Unix-like commands: ls, lsd, mkdir, mv, cat, pwd, cp, rm, diff, grep, which,lns, find (find a.dat), md5sum , base64" && PAUSE && EXIT )
+@DOSKEY ls=dir /B $*
+@DOSKEY lsd=tree /F
+@DOSKEY mkdir=md $*
+@DOSKEY mv=move $*
+@DOSKEY cat=type $*
+@DOSKEY pwd=cd
+@DOSKEY cp=copy $*
+@DOSKEY rm=del $*
+@DOSKEY diff=fc.exe /c /N /A $1 $2
+@DOSKEY grep=findstr /N /I /R /S $*
+@DOSKEY which=where $*
+@DOSKEY lns=junction64.exe $2 $1
+@DOSKEY find=dir $* /s /b 
+@DOSKEY md5sum=certutil -hashfile $1 MD5
+@DOSKEY base64=del /q %DriveName%\Temp\base64.txt $t certutil -encode $1 %DriveName%\Temp\base64.txt $t type %DriveName%\Temp\base64.txt
+EXIT
+
+
+
+
+ 
+
