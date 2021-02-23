93feafff712551495455674b246671cde8c1ea93
Mu <caesarmu@users.noreply.github.com>
2021-02-23 20:11:09 +0800

    Update gjf2xyz.bat

--- gjf2xyz/gjf2xyz.bat
+++ gjf2xyz/gjf2xyz.bat
@@ -2,9 +2,9 @@
 SET gjfin=%1
 SET gjfdir=%~dp1
 SET gjfinbase=%~n1
-gawk.exe < %gjfin% "(NF==4 && length($1)<3  && $1~/^[A-Za-z]+$/ && $2~/^[0-9.-]+$/ && $3~/^[0-9.-]+$/ && $4 ~/^[0-9.-]+$/){natom=natom+1}END{print natom}"  > %gjfdir%%gjfinbase%.xyz
+busybox64.exe awk < %gjfin% "(NF==4 && length($1)<3  && $1~/^[A-Za-z]+$/ && $2~/^[0-9.-]+$/ && $3~/^[0-9.-]+$/ && $4 ~/^[0-9.-]+$/){natom=natom+1}END{print natom}"  > %gjfdir%%gjfinbase%.xyz
 ECHO.  >> %gjfdir%%gjfinbase%.xyz 
-gawk.exe < %gjfin% "(NF>3 && length($1)<3  && $1~/^[A-Za-z]+$/ && $2~/^[0-9.-]+$/ && $3~/^[0-9.-]+$/ && $4 ~/^[0-9.-]+$/)"  >> %gjfdir%%gjfinbase%.xyz 
+busybox64.exe awk < %gjfin% "(NF>3 && length($1)<3  && $1~/^[A-Za-z]+$/ && $2~/^[0-9.-]+$/ && $3~/^[0-9.-]+$/ && $4 ~/^[0-9.-]+$/)"  >> %gjfdir%%gjfinbase%.xyz 
 ECHO %gjfdir%%gjfinbase%.xyz generated!
 EXIT 
 
